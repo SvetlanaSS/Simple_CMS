587bbf3eb72cb33737314a8498e603a7d6ca2f38
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-05-02 12:06:01 +0200

    Login form

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+  require_once "dbconfig.php";
+
+  $link = db_connect();
+
+  include_once "partials/head.php";
+  include_once "partials/navmenu.php";
+  include_once "partials/login.php";
+  include_once "partials/footer.php";
+?>
