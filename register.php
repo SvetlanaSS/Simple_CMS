487999802a2665a152a33cc800bf9b76e6a0c954
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-05-02 11:26:02 +0200

    Registrations form

--- register.php
+++ register.php
@@ -1,3 +1,10 @@
 <?php
-include_once 'partials/head.php';
-include_once 'partials/navmenu.php';
+  require_once "dbconfig.php";
+
+  $link = db_connect();
+
+  include_once "partials/head.php";
+  include_once "partials/navmenu.php";
+  include_once "partials/register.php";
+  include_once "partials/footer.php";
+?>
