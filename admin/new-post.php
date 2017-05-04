5816e7e4d0a6f4db7bfde941198cb22336e05978
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-05-04 19:27:57 +0200

    WIP add post

---
+++ admin/new-post.php
@@ -0,0 +1,17 @@
+<?php
+  require_once '../includes/Database.php';
+  require_once '../includes/error.php';
+  require_once '../includes/helperFunctions.php';
+  require_once '../includes/Article.php';
+
+  $database = new Database();
+  $articleModel = new Article($database);
+
+  $host = $_SERVER['HTTP_HOST'];
+  $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\admin');
+  $redirect = '';
+
+  $articleModel->addArticle();
+  $redirect = 'myPage.php?addPostSuccess=true';
+  header("Location: http://$host$uri/$redirect");
+?>
--- myPage.php
+++ myPage.php
@@ -1,3 +1,22 @@
 <?php
-include_once 'partials/head.php';
-include_once 'partials/navmenuLoggedIn.php';
+  require_once 'includes/Database.php';
+  require_once 'includes/Article.php';
+  require_once 'includes/templates/ArticleTemplate.php';
+
+  $database = new Database();
+  $articleModel = new Article( $database );
+  $articleView = new ArticleTemplate();
+
+  include_once "partials/head.php";
+  include_once "partials/navmenu.php";
+?>
+  <?php if(isset($_GET['addPostSuccess'])):?>
+    <div class="alert alert-info">
+      <p>Ditt inlägg har lagts</p>
+    </div>
+  <?php endif;?>
+
+<?php
+
+  include_once "partials/footer.php";
+?>
